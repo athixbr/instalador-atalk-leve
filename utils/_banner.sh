b794c684fd2105799bda6470548621a16ee6af14
athix <35705895+athixbr@users.noreply.github.com>
2026-05-19 22:55:52 -0300

    feat: instalador atalk leve - backend build local + sem migrate/seed automatico

---
+++ utils/_banner.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+#
+# Print banner art.
+
+print_banner() {
+
+  clear
+
+  printf "\n\n"
+
+printf "${CYAN_LIGHT}";
+
+printf ${CYAN_LIGHT}"    _     _____  _    _     _  __\n";
+printf ${CYAN_LIGHT}"   /_\\   |_   _|/_\\  | |   | |/ /\n";
+printf ${CYAN_LIGHT}"  / _ \\    | | / _ \\ | |   | ' / \n";
+printf ${CYAN_LIGHT}" / ___ \\   | |/ ___ \\| |___| . \\ \n";
+printf ${CYAN_LIGHT}"/_/   \\_\\  |_/_/   \\_\\_____|_|\\_\\\n";
+printf ${CYAN_LIGHT}"                                   \n";
+printf ${CYAN_LIGHT}"       [ LEVE - 1GB - BUILD LOCAL ]\n";
+
+printf "${CYAN_LIGHT}";
+
+  printf "${NC}";
+
+  printf "\n"
+}
